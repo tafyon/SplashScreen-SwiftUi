9811a97b5badd4b2454dbe2fa1307529be9c4bfa
Tamer Afyon <203502050@ogr.altinbas.edu.tr>
2024-04-28 01:18:31 +0300

    Initial Commit

---
+++ SplashScreen-SwiftUi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SplashScreen-SwiftUi
+//
+//  Created by Tamer Afyon on 28.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SplashScreen-SwiftUi/SplashScreen_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashScreen_SwiftUiApp.swift
+//  SplashScreen-SwiftUi
+//
+//  Created by Tamer Afyon on 28.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SplashScreen_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
